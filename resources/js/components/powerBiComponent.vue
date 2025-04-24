5415d12fb7e8334081d803639181700a740c3cc6
SJVSVD <106128546+SJVSVD@users.noreply.github.com>
2025-04-23 20:55:35 -0400

    commit

--- resources/js/components/powerBiComponent.vue
+++ resources/js/components/powerBiComponent.vue
@@ -4,16 +4,38 @@
       <div class="col-lg-12">
         <div class="card">
           <div class="card-body p-4" style="min-height: 400px;">
-            <embed
-              src="https://app.powerbi.com/view?r=eyJrIjoiZTk5NzliNjMtNGYzMS00YTFjLTgzZDItZDkwZWUzNzk5ZWJkIiwidCI6IjAyNjI1Njc2LTMyMjctNDQwYS05YzY4LWJiNmQyOWRlNDIwNiIsImMiOjR9"
-              width="100%"
-              height="800px"
-              type="text/html"
-            />
+            <div v-if="clipboardSupported">
+              <iframe
+                class="w-100"
+                style="max-width: 100%; height: 800px;"
+                src="https://app.powerbi.com/view?r=eyJrIjoiZTk5NzliNjMtNGYzMS00YTFjLTgzZDItZDkwZWUzNzk5ZWJkIiwidCI6IjAyNjI1Njc2LTMyMjctNDQwYS05YzY4LWJiNmQyOWRlNDIwNiIsImMiOjR9"
+                frameborder="0"
+                allowFullScreen="true"
+              ></iframe>
+            </div>
+            <div v-else class="text-center text-danger">
+              Este navegador no es compatible con algunas funciones requeridas por Power BI. Intenta con otro navegador actualizado como Chrome o Edge.
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipboardSupported: true,
+    };
+  },
+  mounted() {
+    if (typeof window.ClipboardItem === "undefined") {
+      console.warn("ClipboardItem no está definido en este navegador.");
+      this.clipboardSupported = false;
+    }
+  },
+};
+</script>
 
