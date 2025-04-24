0db78f9e1349dba1527b30f5b4ec435f630ec8fe
SJVSVD <106128546+SJVSVD@users.noreply.github.com>
2025-04-23 20:00:48 -0400

    commit

--- resources/js/components/powerBiComponent.vue
+++ resources/js/components/powerBiComponent.vue
@@ -5,12 +5,13 @@
         <div class="card">
           <div class="card-body p-4" style="min-height: 400px;">
             <iframe
-              class="w-100"
-              style="max-width: 100%; height: 800px;"
-              src="http://app.powerbi.com/view?r=eyJrIjoiZTk5NzliNjMtNGYzMS00YTFjLTgzZDItZDkwZWUzNzk5ZWJkIiwidCI6IjAyNjI1Njc2LTMyMjctNDQwYS05YzY4LWJiNmQyOWRlNDIwNiIsImMiOjR9"
-              frameborder="0"
-              allowFullScreen="true"
-            ></iframe>
+            class="w-100"
+            style="max-width: 100%; height: 800px;"
+            src="https://app.powerbi.com/view?r=eyJrIjoiZTk5NzliNjMtNGYzMS00YTFjLTgzZDItZDkwZWUzNzk5ZWJkIiwidCI6IjAyNjI1Njc2LTMyMjctNDQwYS05YzY4LWJiNmQyOWRlNDIwNiIsImMiOjR9"
+            frameborder="0"
+            allowFullScreen="true"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; allow-same-origin"
+          ></iframe>
           </div>
         </div>
       </div>
