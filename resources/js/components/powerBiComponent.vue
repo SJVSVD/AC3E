0cd85f074e02d349698cf59acd678c97eb5272a5
SJVSVD <106128546+SJVSVD@users.noreply.github.com>
2025-04-23 20:52:45 -0400

    commit

--- resources/js/components/powerBiComponent.vue
+++ resources/js/components/powerBiComponent.vue
@@ -4,13 +4,12 @@
       <div class="col-lg-12">
         <div class="card">
           <div class="card-body p-4" style="min-height: 400px;">
-            <iframe
-            class="w-100"
-            style="max-width: 100%; height: 800px;"
-            src="https://app.powerbi.com/view?r=eyJrIjoiZTk5NzliNjMtNGYzMS00YTFjLTgzZDItZDkwZWUzNzk5ZWJkIiwidCI6IjAyNjI1Njc2LTMyMjctNDQwYS05YzY4LWJiNmQyOWRlNDIwNiIsImMiOjR9"
-            frameborder="0"
-            allowFullScreen="true"
-          ></iframe>
+            <embed
+              src="https://app.powerbi.com/view?r=eyJrIjoiZTk5NzliNjMtNGYzMS00YTFjLTgzZDItZDkwZWUzNzk5ZWJkIiwidCI6IjAyNjI1Njc2LTMyMjctNDQwYS05YzY4LWJiNmQyOWRlNDIwNiIsImMiOjR9"
+              width="100%"
+              height="800px"
+              type="text/html"
+            />
           </div>
         </div>
       </div>
